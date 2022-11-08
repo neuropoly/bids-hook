4f32c07f77e5e0d6835a522243ab4c41e8fa9d78
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2022-11-08 16:26:51 -0500

    trivial server
    Client says:
    $ printf 'GET / HTTP/1.1\r\nHost: localhost\r\n\r\n' | nc 127.0.0.1 2845
    HTTP/1.1 200 OK
    Date: Tue, 08 Nov 2022 21:28:11 GMT
    Content-Length: 0
    
    Server says:
    $ ./bids-hook
    2022/11/08 16:28:11 handle: got request

--- bids-hook.go
+++ bids-hook.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
-	log.Println("main: hello")
+	server := &http.Server{
+		Addr:           "127.0.0.1:2845",
+		Handler:        http.HandlerFunc(handle),
+		ReadTimeout:    1 * time.Second,
+		MaxHeaderBytes: 4 << 10,
+	}
+	log.Fatal(server.ListenAndServe())
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	log.Println("handle: got request")
 }
