e21a034c9bf66e8c8ff3ffa6753e555cc195c224
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2022-12-06 12:42:23 -0500

    add router
    Supports GET, HEAD, POST, OPTIONS.
    The POST handler just logs the request and responds with 202.
    
    Client says:
    $ (printf 'POST /bids-hook HTTP/1.1\r\n'
       printf 'Host: 127.0.0.1:2845\r\n'
       printf 'Content-Length: 2\r\n'
       printf 'Content-Type: application/json\r\n'
       printf '\r\n'
       printf '{}'
      ) | nc 127.0.0.1 2845
    
    Server says:
    HTTP/1.1 202 Accepted
    Content-Type: text/plain; charset=utf-8
    Date: Tue, 06 Dec 2022 17:46:16 GMT
    Content-Length: 0

--- bids-hook.go
+++ bids-hook.go
@@ -3,19 +3,83 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 )
 
+var (
+	// 127.0.0.1 is localhost, and 2845 is 0xB1D
+	// this should be entered as-is in Gitea to configure the webhook
+	bidsHookUrl = urlMustParse("http://127.0.0.1:2845/bids-hook")
+
+	allowedMethods = strings.Join([]string{
+		http.MethodGet,
+		http.MethodHead,
+		http.MethodPost,
+		http.MethodOptions,
+	}, ", ")
+)
+
+func urlMustParse(rawURL string) *url.URL {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return u
+}
+
 func main() {
 	server := &http.Server{
-		Addr:           "127.0.0.1:2845",
-		Handler:        http.HandlerFunc(handle),
+		Addr:           bidsHookUrl.Host,
+		Handler:        http.HandlerFunc(router),
 		ReadTimeout:    1 * time.Second,
 		MaxHeaderBytes: 4 << 10,
 	}
+	log.Printf("main: listening on %q", bidsHookUrl)
 	log.Fatal(server.ListenAndServe())
 }
 
-func handle(w http.ResponseWriter, r *http.Request) {
-	log.Println("handle: got request")
+// router checks the host, method and target of the request,
+// and delegates meaningful requests to postHandler.
+func router(w http.ResponseWriter, r *http.Request) {
+	if host := r.Host; host != bidsHookUrl.Host {
+		log.Printf("router: wrong host: %q", host)
+		http.Error(w, "421 wrong host", http.StatusMisdirectedRequest)
+		return
+	}
+	method := r.Method
+	target := r.RequestURI
+	log.Printf("router: got request for %q %q", method, target)
+	switch method {
+	case http.MethodPost:
+		if target == bidsHookUrl.RequestURI() {
+			postHandler(w, r)
+			return
+		}
+	case http.MethodGet, http.MethodHead:
+		if target == bidsHookUrl.RequestURI() {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	case http.MethodOptions:
+		if target == bidsHookUrl.RequestURI() {
+			w.Header().Set("Allow", allowedMethods)
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	default:
+		http.Error(w, "501 wrong method", http.StatusNotImplemented)
+		return
+	}
+	http.Error(w, "404 not found", http.StatusNotFound)
+	return
+}
+
+// postHandler deals with requests that have successfully passed
+// through the router based on their host, method and target.
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("postHandler: got request")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusAccepted)
 }
